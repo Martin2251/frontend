6afd74808352f7f8e07d962d808850986750d019
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-07 07:25:36 +0100

    using the thunk way

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useGetAllProductsQuery } from '../features/productsApi'
+import { useSelector } from 'react-redux'
 
 export const Home = () => {
+  const {items, status} = useSelector(state => state.products)
   const {data, error, isLoading} = useGetAllProductsQuery()
   return (
     <div><h2>Home</h2></div>
