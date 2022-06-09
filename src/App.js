52eb50076566fc9759aaba5bbdaffdc452320191
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-09 08:28:52 +0100

    start of toastify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import {BrowserRouter, Route, Switch, Redirect, NavLink} from "react-router-dom"
 import { Home } from './components/Home';
 import NotFound from './components/NotFound';
+import {ToastContainer} from "react-toastify";
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <ToastContainer />
       <NavBar />
       <Switch>
         <Route path="/cart" exact component={Cart} />
