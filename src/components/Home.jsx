ee1afde7160c30e85fc1a5db8bf124fb206ca4d2
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-10 07:29:27 +0100

    modifoed toastify and added local storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,18 @@
 import { useDispatch } from "react-redux";
 import React from "react";
-
+import { useHistory } from "react-router-dom";
 import { useGetAllProductsQuery } from "../features/productsApi";
 import { addToCart } from "../features/cartSlice";
+
 export const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
   const dispatch = useDispatch();
+  const history = useHistory();
 
-  const handleAddToCart  = (product) => {
-dispatch(addToCart(product))
-  }
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+    history.push("/Cart");
+  };
   return (
     <div className="home-container">
       {isLoading ? (
@@ -23,12 +26,14 @@ dispatch(addToCart(product))
             {data?.map((product) => (
               <div key={product.id} className="product">
                 <h3>{product.name}</h3>
-                <img src={product.image} alt={product.name}/>
+                <img src={product.image} alt={product.name} />
                 <div className="details">
                   <span>{product.desc}</span>
                   <span className="price">${product.price}</span>
                 </div>
-                <button onClick={() => handleAddToCart(product)}>Add to cart</button>
+                <button onClick={() => handleAddToCart(product)}>
+                  Add to cart
+                </button>
               </div>
             ))}
           </div>
