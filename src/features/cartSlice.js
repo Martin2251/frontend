cf6f794a9504fab563624e4e42559e956cc4d643
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-13 07:42:41 +0100

    decreased and increased cart

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -30,23 +30,44 @@ const cartSlice = createSlice({
           position: "bottom-left",
         });
       }
-      localStorage.setItem("cartItems", JSON.stringify(state.cartItems))
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
-    removeFromCart(state,action){
-        const nextCartItems = state.cartItems.filter(
+    removeFromCart(state, action) {
+      const nextCartItems = state.cartItems.filter(
         (cartItem) => cartItem.id !== action.payload.id
+      );
+      state.cartItems = nextCartItems;
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
+      toast.error(`${action.payload.name} removed from cart`, {
+        position: "bottom-left",
+      });
+    },
+    decreaseCart(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (cartItem) => cartItem.id === action.payload.id
+      );
+
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+        toast.info(`Decreased${action.payload.name} card quantity`, {
+          position: "bottom-left",
+        });
+      } else if (state.cartItems[itemIndex].cartQuantity === 1) {
+        const nextCartItems = state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
         );
         state.cartItems = nextCartItems;
-        localStorage.setItem("cartItems", JSON.stringify(state.cartItems))
 
         toast.error(`${action.payload.name} removed from cart`, {
-            position: "bottom-left",
-          });
-    }
-
+          position: "bottom-left",
+        });
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
   },
 });
 
-export const { addToCart,removeFromCart } = cartSlice.actions;
+export const { addToCart,removeFromCart, decreaseCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
